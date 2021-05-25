8eedc9350337709adf45c3aeab22b7532a23c2e6
Soumay kanti Das <anikds2859@outlook.com>
2021-05-25 11:59:47 +0600

    Add project files.

---
+++ OtherToString/Program.cs
@@ -0,0 +1,19 @@
+﻿//keyword_string.cs:
+
+using System;
+
+namespace OtherToString
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string a = "\u0068ello ";
+            string b = "world";
+            Console.WriteLine(a + b);
+            Console.WriteLine(a + b == "hello World");
+
+
+        }
+    }
+}
